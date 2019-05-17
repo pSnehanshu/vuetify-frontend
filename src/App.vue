0bd23b7fb8ba2ce2c368e16d7f4be46c3895efaa
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-05-17 21:41:48 +0530

    Removed demo content

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-toolbar app></v-toolbar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-content></v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+  components: {},
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
